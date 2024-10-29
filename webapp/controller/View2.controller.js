d4f8b3276be8264c075307196327f2127a0e72f5
Sudheer Gujjarlapudi <sudheergujjarlapudi@gmail.com>
2024-10-29 06:53:18 +0000

    thirdCommit

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,31 +1,31 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-	"sap/m/MessageToast"
+    "sap/m/MessageToast"
 ], function (
     Controller,
-	JSONModel,
-	MessageToast
+    JSONModel,
+    MessageToast
 ) {
     "use strict";
 
     return Controller.extend("project2.controller.View2", {
         onInit() {
             var oData = new JSONModel({
-                Names: [{ name: "Sudheer", id: 64, parentDetails: [{ Parentname: "suresh", age: 48 }, { Parentname: "kumari", age: 43 }] ,Status:""},
-                { name: "ramya", id: 55, parentDetails: [],Status:"" }, // Example with no parentDetails
-                { name: "lakshmi", id: 39, parentDetails: [{ Parentname: "john", age: 35 }] ,Status:""},
-                { name: "vanitha", id: 57 , parentDetails: [] ,Status:""}
-                ]
+                Names: [{ name: "Sudheer", id: 64, parentDetails: [{ Parentname: "suresh", age: 48 }, { Parentname: "kumari", age: 43 }], Status: "" },
+                { name: "ramya", id: 55, parentDetails: [], Status: "" },
+                { name: "lakshmi", id: 39, parentDetails: [{ Parentname: "john", age: 35 }], Status: "" },
+                { name: "vanitha", id: 57, parentDetails: [], Status: "" }
+                ],
+                TableData: []
             })
             this.getView().setModel(oData);
-            var oFirstItemPath = "/Names/0";
-            this.byId("idsimpleform").bindElement({ path: oFirstItemPath });
         },
         onStandardListItemPress(oEvent) {
             this.byId("1").setLayout("TwoColumnsMidExpanded")
-            this.oArgue= oEvent.getSource().getBindingContext().getPath()
+            this.oArgue = oEvent.getSource().getBindingContext().getPath()
             this.byId("idsimpleform").bindElement(this.oArgue)
+            this.byId("table1").bindElement(this.oArgue)
         },
         onAdding() {
             var id = this.byId("id1").getValue()
@@ -37,36 +37,50 @@ sap.ui.define([
         },
         onTabAdd(oEvent) {
             var sPath = oEvent.getSource().getParent().getBindingContext().getPath() + '/parentDetails';
-            this.byId("1").setLayout("ThreeColumnsEndExpanded")
             var oModel = this.getView().getModel();
             var aParentDetails = oModel.getProperty(sPath);
             aParentDetails.push({ Parentname: "sunny", age: 22 });
             oModel.setProperty(sPath, aParentDetails);
         },
-        close(oEvent){
-           var id= oEvent.getSource().getId()
-           switch(true){
-            case id.endsWith("22"):
-                this.byId("1").setLayout("OneColumn")
-                break
-            case id.endsWith("33"):
-                this.byId("1").setLayout("TwoColumnsMidExpanded")
-                break
-            
-           }
+        close(oEvent) {
+            var id = oEvent.getSource().getId()
+            switch (true) {
+                case id.endsWith("22"):
+                    this.byId("1").setLayout("OneColumn")
+                    break
+                case id.endsWith("33"):
+                    this.byId("1").setLayout("TwoColumnsMidExpanded")
+                    break
+
+            }
         },
-        onButtonPress1(oEvent){
-            var sPath = this.oArgue+"/Status"
+        onButtonPress1(oEvent) {
+            var sPath = this.oArgue + "/Status"
+            this.byId("1").setLayout("ThreeColumnsEndExpanded")
             var oModel = this.getView().getModel();
-            oModel.setProperty(sPath,"Approved");
-                MessageToast.show("Data Approved")
-                console.log(oModel.getProperty(sPath))
-           },
-           onButtonPress2(oEvent){
-            var sPath = this.oArgue+"/Status"
+            oModel.setProperty(sPath, "Approved");
+            MessageToast.show("Data Approved")
+            var array = oModel.getProperty("/TableData")
+            var oData = oModel.getProperty(this.oArgue)
+            var exists = array.some(item => item.id === oData.id);
+            if (!exists) {
+                array.push(oData);
+            }
+            this.getView().getModel().refresh(true);
+        },
+        onButtonPress2(oEvent) {
+            var sPath = this.oArgue + "/Status"
+            this.byId("1").setLayout("ThreeColumnsEndExpanded")
             var oModel = this.getView().getModel();
-            oModel.setProperty(sPath,"Rejected");
-                MessageToast.show("Data Rejected")
-           },
+            oModel.setProperty(sPath, "Rejected");
+            MessageToast.show("Data Rejected")
+            var array = oModel.getProperty("/TableData")
+            var oData = oModel.getProperty(this.oArgue)
+            var exists = array.some(item => item.id === oData.id);
+            if (!exists) {
+                array.push(oData);
+            }
+            this.getView().getModel().refresh(true);
+        },
     });
 });
